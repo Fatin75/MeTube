7fb994e3d2e105aa2e69fbb22cb918eb35eff444
FatinNur <70317137+FatinNur@users.noreply.github.com>
2022-05-01 01:05:31 +0600

    Update script.js

--- script.js
+++ script.js
@@ -6,20 +6,3 @@ menuIcon.onclick = function(){
     sidebar.classList.toggle("small-sidebar");
     container.classList.toggle("large-container");
 }
-
-swal({
-  title: "Created by Fatin",
-  text: "What's your name?",
-  content:  {
-    element: "input",
-    attributes: {
-      placeholder: "Type your name",
-      type: "text",
-    },
-  },
-  dangerMode: true,
-  buttons: true,
-})
-.then((value) => {
-  swal(`Thank you ${value} for visiting.`);
-});
